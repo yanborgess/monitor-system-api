0528464382246df21aac70d80542b7185c78154b
yanborgess <yan.borges.areda@gmail.com>
2026-05-06 19:13:29 -0300

    fix: add cascade delete to device logs

--- src/main/java/com/yan/security/monitorSystem/models/DeviceLog.java
+++ src/main/java/com/yan/security/monitorSystem/models/DeviceLog.java
@@ -2,6 +2,8 @@ package com.yan.security.monitorSystem.models;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.time.LocalDateTime;
 
@@ -20,6 +22,7 @@ public class DeviceLog {
 
     @ManyToOne
     @JoinColumn(name = "device_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Device device;
 
 
