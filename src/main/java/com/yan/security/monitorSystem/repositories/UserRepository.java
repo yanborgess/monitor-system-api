d249d62bf449d0e83e1ff9032e85ca5704935d13
yanborgess <yan.borges.areda@gmail.com>
2026-05-07 20:35:43 -0300

    feat(auth): implement UserRepository for login lookup

--- src/main/java/com/yan/security/monitorSystem/repositories/UserRepository.java
+++ src/main/java/com/yan/security/monitorSystem/repositories/UserRepository.java
@@ -1,4 +1,9 @@
 package com.yan.security.monitorSystem.repositories;
 
-public class UserRepository {
+import com.yan.security.monitorSystem.models.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    UserDetails findByLogin(String login);
 }
